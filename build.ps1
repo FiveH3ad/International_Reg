6dd72140823625c429a99fdb6870d12580bf4d92
Nils Frischknecht <nils.frischknecht@swissrds.ch>
2023-09-05 11:05:12 +0200

    default

--- build.ps1
+++ build.ps1
@@ -6,6 +6,10 @@ Set-Location $PSScriptRoot
 
 connect-azaccount
 
+if(Test-Path BechtleSchweizWindowsVirtualMachineConfiguration.zip){
+    git rm BechtleSchweizWindowsVirtualMachineConfiguration.zip -f 
+}
+
 . .\vmdefaults.ps1
 
 $params = @{
@@ -20,7 +24,7 @@ $configuration = @{
     Description = 'Sets Default Regional Settings and installs SNMP Service'
     PolicyID = New-GUID
     PolicyVersion = '1.0.0'
-    ContentUri = 'https://github.com/FiveH3ad/International_Reg/raw/main/BechtleSchweizVirtualMachineConfiguration.zip'
+    ContentUri = 'https://github.com/FiveH3ad/International_Reg/raw/main/BechtleSchweizWindowsVirtualMachineConfiguration.zip'
     Path = '.\policy'
     Platform = 'Windows'
     Mode = 'Applyandautocorrect'
@@ -29,10 +33,20 @@ $configuration = @{
 
 New-GuestConfigurationPackage @params
 
+Remove-Item .\vmdefaults -Recurse -Force
+
+git add BechtleSchweizWindowsVirtualMachineConfiguration.zip
+git commit -m 'default'
+git push
+
+Start-Sleep -Seconds 20
+
 New-GuestConfigurationPolicy @Configuration
 
 $policyfile = (Get-Childitem .\policy).fullname
 
 New-AzPolicyDefinition -Name 'Bechtle Schweiz Default Windows Virtual Machine Congfiguration' -Policy $policyfile
 
-Remove-Item .\vmdefaults -Recurse -Force
+git add *
+git commit -m 'default'
+git push
